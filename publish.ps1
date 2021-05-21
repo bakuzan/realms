46de967878e5535f4173fddad3b27bae13dc772e
bakuzan <imannoyed4days@hotmail.co.uk>
2021-05-21 13:30:49 +0100

    Rename project names

--- publish.ps1
+++ publish.ps1
@@ -7,12 +7,12 @@ function Get-OuputColour($v) { if ($v) { return "Blue" } else { return "Green" }
 
 
 Write-Host "";
-$user_input = Read-Host -Prompt "Do you want to release Wiki after publishing?"
+$user_input = Read-Host -Prompt "Do you want to release Realms after publishing?"
 Write-Host "";
 
 $release = $false;
 $src = Join-Path (Get-ScriptDirectory) "\bin\Release\netcoreapp3.1\publish\*";
-$dst = "C:\inetpub\wwwroot\wiki";
+$dst = "C:\inetpub\wwwroot\realms";
 
 if (@("true", "false", "yes", "no", "y", "n") -contains $user_input) {
     $release = @("true", "yes", "y") -contains $user_input
@@ -28,7 +28,7 @@ else {
 dotnet publish --configuration Release;
 
 Write-Host "";
-Write-Host "Wiki published." -ForegroundColor (Get-OuputColour $release);
+Write-Host "Realms published." -ForegroundColor (Get-OuputColour $release);
 Write-Host "";
 
 if ($release) {
@@ -37,13 +37,12 @@ if ($release) {
     Copy-Item -Path $src -Destination $dst -Exclude "*.db" -Recurse -Container -Force
     
     Write-Host "";
-    Write-Host "Wiki released." -ForegroundColor "Green";
+    Write-Host "Realms released." -ForegroundColor "Green";
     Write-Host "";
 }
 
 # If running in the console, wait for input before closing.
-if ($Host.Name -eq "ConsoleHost")
-{
+if ($Host.Name -eq "ConsoleHost") {
     Write-Host "Press any key to continue..."
     $Host.UI.RawUI.FlushInputBuffer()   # Make sure buffered input doesn't "press a key" and skip the ReadKey().
     $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp") > $null
